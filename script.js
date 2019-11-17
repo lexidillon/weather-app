009374d41f8de77f70401bcd74f198083193692a
Lexi Dillon <and4bb@virginia.edu>
2019-11-17 21:38:07 +0000

    added function

--- script.js
+++ script.js
@@ -153,3 +153,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
+
+showTemperature();
